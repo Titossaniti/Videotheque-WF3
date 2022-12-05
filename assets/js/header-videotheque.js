d23747c31112a9bec29b788d05362368c1bc9a99
Your Name~clear <you@example.com>
2022-12-05 16:54:08 +0100

    modif lien logo header

--- assets/js/header-videotheque.js
+++ assets/js/header-videotheque.js
@@ -2,5 +2,5 @@ let injectionHeader = document.getElementById("header");
 
 injectionHeader.innerHTML =`<section class="container"><div class="row d-flex justify-content-between">
 
-<div><p><a href="index.html" class="fw-bold fs-2 text-decoration-none text-dark" id="logo">Ma Vidéothèque</a></p></div>
+<div><p><a href="https://titossaniti.github.io/Videotheque-WF3/index.html" class="fw-bold fs-2 text-decoration-none text-dark" id="logo">Ma Vidéothèque</a></p></div>
     </div></section>`
